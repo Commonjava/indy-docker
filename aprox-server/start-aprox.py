cd74738497c76cb50a122cc453bcd6fb64e3516c
John Casey <jdcasey@commonjava.org>
2015-03-16 17:51:54 -0500

    more debugging

--- aprox-server/start-aprox.py
+++ aprox-server/start-aprox.py
@@ -33,6 +33,8 @@ def runIn(cmd, workdir, fail_message='Error running command', fail=True):
 
 def move_and_link(src, target, replaceIfExists=False):
   print "Source: %s\nTarget: %s" % (src, target)
+  print "Existing target listing:\n"
+  run("tree -a %s" % target)
   
   srcParent = os.path.dirname(src)
   if not os.path.isdir(srcParent):
